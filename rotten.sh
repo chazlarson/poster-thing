841a042c00e59cbf28429baca77d6e0af11d80c1
Chaz Larson <chaz@chazlarson.com>
2022-11-10 17:18:14 -0600

    Update rotten.sh

--- rotten.sh
+++ rotten.sh
@@ -32,6 +32,8 @@ LOGOSCALE=80%
 BGCOLOR="#FD3200"
 BOTTOMCOLOR="white"
 
-./poster-gen.sh -f ${FILENAME} -t "${TEXT}" -l ${LOGOFILE} -g ${LOGOGEO} -s ${LOGOSCALE} -c ${BGCOLOR} -b ${BOTTOMCOLOR}
+if [ ! -f "output/$FILENAME.png" ]; then
+  ./apostle-gen.sh -f ${FILENAME} -t "${TEXT}" -l ${LOGOFILE} -g ${LOGOGEO} -s ${LOGOSCALE} -c ${BGCOLOR} -b ${BOTTOMCOLOR}
 
-open "output/$FILENAME.png"
+  open "output/$FILENAME.png"
+fi
